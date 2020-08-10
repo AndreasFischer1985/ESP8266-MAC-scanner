a720febacea2fdf4761b694ceba107ca246e5404
Dr. Andreas Fischer <45405681+AndreasFischer1985@users.noreply.github.com>
2020-08-10 21:01:49 +0200

    Coded by Ray Burnette
    Coded by Ray Burnette (cf. https://www.hackster.io/rayburne/esp8266-mini-sniff-f6b93a)

---
+++ Sniffing6.ino
@@ -0,0 +1,45 @@
+// by Ray Burnette 20161013 compiled on Linux 16.3 using Arduino 1.6.12
+
+#include <ESP8266WiFi.h>
+#include "./functions.h"
+
+#define disable 0
+#define enable  1
+// uint8_t channel = 1;
+unsigned int channel = 1;
+
+void setup() {
+  Serial.begin(57600);
+  Serial.printf("\n\nSDK version:%s\n\r", system_get_sdk_version());
+  Serial.println(F("ESP8266 mini-sniff by Ray Burnette http://www.hackster.io/rayburne/projects"));
+  Serial.println(F("Type:   /-------MAC------/-----WiFi Access Point SSID-----/  /----MAC---/  Chnl  RSSI"));
+
+  wifi_set_opmode(STATION_MODE);            // Promiscuous works only with station mode
+  wifi_set_channel(channel);
+  wifi_promiscuous_enable(disable);
+  wifi_set_promiscuous_rx_cb(promisc_cb);   // Set up promiscuous callback
+  wifi_promiscuous_enable(enable);
+}
+
+void loop() {
+  channel = 1;
+  wifi_set_channel(channel);
+  while (true) {
+    nothing_new++;                          // Array is not finite, check bounds and adjust if required
+    if (nothing_new > 200) {
+      nothing_new = 0;
+      channel++;
+      if (channel == 15) break;             // Only scan channels 1 to 14
+      wifi_set_channel(channel);
+    }
+    delay(1);  // critical processing timeslice for NONOS SDK! No delay(0) yield()
+    // Press keyboard ENTER in console with NL active to repaint the screen
+    if ((Serial.available() > 0) && (Serial.read() == '\n')) {
+      Serial.println("\n-------------------------------------------------------------------------------------\n");
+      for (int u = 0; u < clients_known_count; u++) print_client(clients_known[u]);
+      for (int u = 0; u < aps_known_count; u++) print_beacon(aps_known[u]);
+      Serial.println("\n-------------------------------------------------------------------------------------\n");
+    }
+  }
+}
+
